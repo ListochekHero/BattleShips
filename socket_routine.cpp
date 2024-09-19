e0a264832ee9a93551d8be43a7ed773a9a21a856
ListochekHero <artoryasabysswalker@gmail.com>
2024-09-19 18:12:48 +0300

    implementation of Observer pattern and callback for server on connection acceptance

--- socket_routine.cpp
+++ socket_routine.cpp
@@ -31,19 +31,38 @@ int SocketHandler::get_fd() const { return sock_fd; }
 
 void SocketHandler::set_fd(int sock_fd) { this->sock_fd = sock_fd; }
 
-int SocketHandler::accept_connection() {
+void SocketHandler::accept_connection() {
   int client_fd = accept(sock_fd, nullptr, nullptr);
   if (client_fd < 0) {
     throw std::runtime_error("Error while accepting new connection");
   }
-  return client_fd;
+  if (connection_callback) {
+    connection_callback(client_fd);
+  }
 }
 
+void SocketHandler::update(int sock_fd) {
+  if (this->sock_fd == sock_fd) {
+    this->accept_connection();
+  }
+}
+void SocketHandler::set_connection_callback(ConnectionCallback callback) {
+  connection_callback = callback;
+}
 void SocketHandler::make_non_blocking() {
   int flags = fcntl(sock_fd, F_GETFL, 0);
   if (flags == -1 || fcntl(sock_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
     throw std::runtime_error("Error making socket non-blocking");
   }
+}
+
+void SocketIOHandler::read_from_socket() {
+  char buffer[BUFF_SIZE];
+  int n = read(this->get_fd(), buffer, BUFF_SIZE);
+  if (n < 0) {
+    // TODO
+  }
+  safe_buffer.assign(buffer);
 }
 
 EpollHandler::EpollHandler() {
@@ -56,21 +75,32 @@ EpollHandler::~EpollHandler() { close(epollfd); }
 void EpollHandler::add_socket(int sock_fd) {
   struct epoll_event event;
   event.data.fd = sock_fd;
-  event.events = EPOLLIN;
+  event.events = EPOLLIN | EPOLLET;
   if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sock_fd, &event) == -1) {
     throw std::runtime_error("Error adding socket to epoll");
   }
 }
 
-std::vector<int> EpollHandler::wait_for_events(int max_events) {
+void EpollHandler::attach(int sock_fd, Observer *observer) {
+  observers[sock_fd] = observer;
+};
+void EpollHandler::detach(Observer *observer) {
+  observers.erase(std::remove(observers.begin(), observers.end(), observer),
+                  observers.end());
+};
+
+void EpollHandler::wait_for_events(int max_events) {
   std::vector<struct epoll_event> events(max_events);
   int n = epoll_wait(epollfd, events.data(), max_events, -1);
 
-  std::vector<int> ready_fds;
   for (int i = 0; i < n; ++i) {
-    ready_fds.push_back(events[i].data.fd);
+    int sock_fd = events[i].data.fd;
+    if (observers.find(sock_fd) != observers.end()) notify(sock_fd);
   }
-  return ready_fds;
 }
-
+void EpollHandler::notify(int sock_fd) {
+  if (observers[sock_fd]) {
+    observers[sock_fd]->update(sock_fd);
+  }
+}
 }  // namespace BattleShipsMain
--- socket_routine.h
+++ socket_routine.h
@@ -7,36 +7,54 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 
+#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <map>
 #include <vector>
 
-#define BACKLOG 10
+#include "interfaces.h"
 
+#define BACKLOG 10
+#define BUFF_SIZE 1024
 namespace BattleShipsMain {
 
-class SocketHandler {
+class SocketHandler : public Observer {
  public:
+  using ConnectionCallback = std::function<void(int)>;
   SocketHandler(int port);
   ~SocketHandler();
   int get_fd() const;
   void set_fd(int sock_fd);
-  int accept_connection();
-
+  void accept_connection();
+  void update(int sock_fd) override;
+  void set_connection_callback(ConnectionCallback callback);
  private:
   int sock_fd;
+  ConnectionCallback connection_callback;
   void make_non_blocking();
 };
 
-class SocketIOHandler : public SocketHandler{
-
+class SocketIOHandler : private SocketHandler, Observer {
+  public:
+  void read_from_socket();
+  private:
+    std::string permanent_buffer;
 };
-class EpollHandler {
+class EpollHandler : public ObservableSu8ject {
  public:
   EpollHandler();
   ~EpollHandler();
   void add_socket(int sock_fd);
-  std::vector<int> wait_for_events(int max_events);
+  void wait_for_events(int max_events);
+  void attach(int sock_fd, Observer *observer) override;
+  void detach(Observer *observer) override;
+
+ private:
   int epollfd;
+  std::map<int, Observer *> observers;
+
+  void notify(int sock_fd) override;
 };
 
 }  // namespace BattleShipsMain
