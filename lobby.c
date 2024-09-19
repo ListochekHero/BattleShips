4a04429425db7a7f7df90dd034bcb0d4e0a3cc3c
ListochekHero <artoryasabysswalker@gmail.com>
2024-09-19 18:09:55 +0300

    Placeholder for lobby implementation

---
+++ lobby.c
@@ -0,0 +1,9 @@
+#include "lobby.h"
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    return 1;
+  }
+   int sock_fd = std::stoi(argv[1]);
+
+}
--- lobby.h
+++ lobby.h
@@ -1,4 +1,11 @@
 #ifndef LOBBY_H
 #define LOBBY_H
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <iostream>
+
 #endif
