11c31911fd342fa2eeb956ade59afb4d0d39bb4d
ListochekHero <artoryasabysswalker@gmail.com>
2024-09-30 19:47:58 +0300

    first approach of a logger

---
+++ logger.cpp
@@ -0,0 +1,35 @@
+#include "logger.h"
+
+namespace BattleShipsMain {
+Logger& Logger::getInstance(const std::string& program_name) {
+  static Logger instance(program_name);
+  return instance;
+}
+void Logger::log(const std::string& message) {
+  std::lock_guard<std::mutex> guard(log_mutex);
+  log_file << "[" << get_current_time() << "] " << "[" << program_name << "] "
+           << "[PID: " << getpid() << "] " << message << std::endl;
+}
+
+Logger::Logger(const std::string& program_name) {
+  log_file.open("log.txt", std::ios::app);
+  if (!log_file.is_open()) {
+    throw std::runtime_error("Could not open log file");
+  }
+}
+Logger::~Logger() {
+  if (log_file.is_open()) {
+    log_file.close();
+  }
+}
+
+std::string Logger::get_current_time() {
+  std::time_t now = std::time(nullptr);
+  std::tm* local_time = std::localtime(&now);
+  std::stringstream time_stream;
+  time_stream << (local_time->tm_year + 1900) << "-" << (local_time->tm_mon + 1)
+              << "-" << local_time->tm_mday << " " << local_time->tm_hour << ":"
+              << local_time->tm_min << ":" << local_time->tm_sec;
+  return time_stream.str();
+}
+}  // namespace BattleShipsMain
--- logger.h
+++ logger.h
@@ -1,18 +1,30 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <unistd.h>
+
+#include <ctime>
 #include <fstream>
 #include <mutex>
+#include <sstream>
 #include <string>
 
 namespace BattleShipsMain {
 class Logger {
  public:
-  static Logger& getInstance();
-    void log(const std::string& message);
+  static Logger& getInstance(const std::string& program_name);
+  void log(const std::string& message);
+
  private:
- std::ofstream log_file;
- std::mutex log_mutex;
+  std::ofstream log_file;
+  std::mutex log_mutex;
+  std::string program_name;
+
+  Logger(const std::string& program_name);
+  ~Logger();
+  std::string get_current_time();
+  Logger(const Logger&) = delete;
+  Logger& operator=(const Logger&) = delete;
 };
 }  // namespace BattleShipsMain
 
