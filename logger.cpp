8899ad936435dab657b175c5608f54d252ba0320
ListochekHero <artoryasabysswalker@gmail.com>
2024-10-07 02:24:53 +0300

    logger now availble with no arguments, added macro to call an instance

--- logger.cpp
+++ logger.cpp
@@ -5,18 +5,26 @@ Logger& Logger::getInstance(const std::string& program_name) {
   static Logger instance(program_name);
   return instance;
 }
+Logger& Logger::getInstance() {
+  if(!instance_created){
+    throw std::runtime_error("Logger must be initialized with a program name first!");
+  }
+  return getInstance("unused");
+}
 void Logger::log(const std::string& message) {
   std::lock_guard<std::mutex> guard(log_mutex);
   log_file << "[" << get_current_time() << "] " << "[" << program_name << "] "
            << "[PID: " << getpid() << "] " << message << std::endl;
 }
 
-Logger::Logger(const std::string& program_name) {
+Logger::Logger(const std::string& program_name) : program_name(program_name) {
   log_file.open("log.txt", std::ios::app);
   if (!log_file.is_open()) {
     throw std::runtime_error("Could not open log file");
   }
+  instance_created = true;
 }
+
 Logger::~Logger() {
   if (log_file.is_open()) {
     log_file.close();
--- logger.h
+++ logger.h
@@ -13,6 +13,7 @@ namespace BattleShipsMain {
 class Logger {
  public:
   static Logger& getInstance(const std::string& program_name);
+  static Logger& getInstance();
   void log(const std::string& message);
 
  private:
@@ -20,12 +21,18 @@ class Logger {
   std::mutex log_mutex;
   std::string program_name;
 
+  inline static bool instance_created = false;
+
   Logger(const std::string& program_name);
+  Logger() = default;
   ~Logger();
   std::string get_current_time();
   Logger(const Logger&) = delete;
   Logger& operator=(const Logger&) = delete;
 };
+
+#define LOG(message_to_log) \
+  BattleShipsMain::Logger::getInstance().log(message_to_log)
 }  // namespace BattleShipsMain
 
 #endif
