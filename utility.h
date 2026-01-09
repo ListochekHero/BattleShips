17b701bd13d165720c495e19fb0fe29d4acde337
ListochekHero <artoryasabysswalker@gmail.com>
2026-01-09 04:52:27 +0200

    Added alias for common return type
    Added alias for common return type of many method - usefull also for explicitly specifying return type of lambdas

--- utility.h
+++ utility.h
@@ -2,7 +2,11 @@
 #define UTILITY_H
 
 #include <filesystem>
+#include <expected>
 namespace bsm {
+
+using Ev = std::expected<void, std::string>;
+
 bool is_file_exist(const std::string& filename);
 
 }  // namespace bsm
