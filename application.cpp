ac721d7fb457ee0850a3f7237c7bf54013acd8c4
ListochekHero <artoryasabysswalker@gmail.com>
2026-01-09 05:13:09 +0200

    Major changes: new inheritance model and socket handling logic, see detailed comment for more info
    Preparation to remove Lobby class, transfered epoll and socket field to basic class under protected scope. Added overloading for init() method. Added method to handle different cases of socket events. run() method now uses new method to handle different sockets. New method to write command for child processes. Modified create_lobby() method to use write_to_child method instead of direct messaging child. Added a lot of logging with new lambda helper. Changed logic of handling client command - removed unnecessary std::expected from map and added error handling for commands.

--- application.cpp
+++ application.cpp
@@ -9,64 +9,95 @@ std::expected<void, std::string> Server::init() {
   this->epoll_handler.add_socket(sockets[0].get());
   return {};
 }
+std::expected<void, std::string> Server::init(SocketHandler& parrent_socket) {
+  this->epoll_handler.init();
+  this->sockets.emplace_back(
+      std::make_unique<SocketHandler>(std::move(parrent_socket)));
+  this->sockets[0].get()->set_socket_type(socket_type_e::IPC);
+  this->epoll_handler.add_socket(sockets[0].get());
+  return {};
+}
 
 void Server::run() {
   while (true) {
     handle_zombie_pocesses();
     epoll_handler.wait_for_events(MAX_EVENTS)
-        .and_then([this](auto events) -> std::expected<void, std::string> {
-          for (SocketHandler* handler : events) {
-            if (handler->get_socket_type() == socket_type_e::SERVER) {
-              handler->accept_connections()
-                  .and_then([this](auto new_clients)
-                                -> std::expected<void, std::string> {
-                    for (std::unique_ptr<SocketHandler>& client : new_clients) {
-                      epoll_handler.add_socket(client.get());
-                    }
-                    sockets.append_range(new_clients | std::views::as_rvalue);
-                    return {};
-                  })
-                  .or_else([](const std::string& e)
-                               -> std::expected<void, std::string> {
-                    LOG(e);
-                    return {};
-                  });
-            } else {
-              auto result{handler->read_user_input()};
-              if (result) handle_client_cmd(*handler, *result);
-            }
-          }
+        .and_then([this](auto&& events) -> Ev {
+          process_events(events);
           return {};
         })
-        .or_else([](const std::string& e) -> std::expected<void, std::string> {
-          LOG(e);
-          return {};
-        });
+        .transform_error(log_and_forward);
   }
 }
 
+void Server::process_events(std::vector<SocketHandler*>& events) {
+  for (SocketHandler* handler : events) {
+    if (handler->get_socket_type() == socket_type_e::SERVER) {
+      process_server_socket(handler);
+    } else {
+      process_client_socket(handler);
+    }
+  }
+}
+
+void Server::process_server_socket(SocketHandler* handler) {
+  handler->accept_connections()
+      .and_then([this](auto&& new_clients) -> Ev {
+        for (auto& client : new_clients) {
+          epoll_handler.add_socket(client.get())
+              .and_then([this, &client]() -> Ev {
+                sockets.emplace_back(std::move(client));
+                return {};
+              })
+              .transform_error([&client](const auto& error) {
+                client.reset(nullptr);
+                return log_and_forward(error);
+              });
+        }
+        return {};
+      })
+      .transform_error(log_and_forward);
+}
+void Server::process_client_socket(SocketHandler* handler) {
+  handler->read_user_input()
+      .and_then([this, handler](auto&& command) -> Ev {
+        handle_client_cmd(*handler, command)
+            .or_else([handler](const auto& error) -> Ev {
+              handler->write_to_user(error).transform_error(log_and_forward);
+              return {};
+            });
+        return {};
+      })
+      .transform_error(log_and_forward);
+}
+
 std::expected<void, std::string> Server::handle_client_cmd(
-    const SocketHandler& client, std::string_view command) {
-  using Handler =
-      std::function<std::expected<void, std::string>(const SocketHandler&)>;
-  LOG(command.data());
+    SocketHandler& client, std::string_view command) {
+  using Handler = std::function<void(SocketHandler&)>;
+  LOG(std::format("Command to handle: {}", command.data()));
   static const std::unordered_map<std::string_view, Handler> command_map = {
-      {"create", [this](const SocketHandler& c) { return create_lobby(c); }},
-      {"close",
-       [this](const SocketHandler& c) { return erase_socket_handler(c); }},
-      {"test", [](const SocketHandler&) -> std::expected<void, std::string> {
-         LOG("test");
-         return {};
-       }}};
+      {"\\create",
+       [this](SocketHandler& client) -> void {
+         create_lobby(client).or_else([&client](const auto& error) -> Ev {
+           log_and_forward(error);
+           client.write_to_user("Cannot create lobby, please try again later")
+               .transform_error(log_and_forward);
+           return {};
+         });
+       }},
+      {"close", [this](SocketHandler& c) -> void { erase_socket_handler(c); }},
+      {"test", [](const SocketHandler&) -> void { LOG("test"); }}};
   if (auto it = command_map.find(command); it != command_map.end()) {
-    return it->second(client);
+    it->second(client);
+    return {};
   } else
     return std::unexpected("Unknown command");
 }
 
-std::expected<void, std::string> Server::create_lobby(
-    const SocketHandler& client) {
-  epoll_handler.remove_socket(&client);
+std::expected<void, std::string> Server::create_lobby(SocketHandler& client) {
+  epoll_handler.remove_socket(&client).transform_error(log_and_forward);
+  if (!client.remove_cloexec().transform_error(log_and_forward))
+    return std::unexpected("Unable to remove cloexec");
   int sv[2];
   if (socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_NONBLOCK, 0, sv) == -1)
     return std::unexpected("Failed to create socketpair");
@@ -83,25 +114,49 @@ std::expected<void, std::string> Server::create_lobby(
     close(sv[0]);
     std::time_t now = std::time(nullptr);
     LOG(std::format("Connection code: \"{}\" - time", now));
-    auto it = lobbies.emplace(now, sv[1]);
-    it.first->second.write_to_user("socket");
-    it.first->second.write_to_user(std::to_string(client.get_socket()));
-    erase_socket_handler(client);
-    it.first->second.write_to_user("conn_code");
-    it.first->second.write_to_user(std::to_string(it.first->first));
+    lobbies.emplace(now, sv[1]);
+    if (!(write_to_child(now, "socket", std::to_string(client.get_socket()))
+              .and_then([this, &client]() -> Ev {
+                erase_socket_handler(client);
+                return {};
+              })
+              .transform_error(log_and_forward)))
+      return std::unexpected("Cant pass socket to child process");
+    if (!(write_to_child(now, "conn_code", std::to_string(now))
+              .transform_error(log_and_forward)))
+      return std::unexpected("Cant send connection code to child process");
     return {};
   }
 }
-std::expected<void, std::string> Server::erase_socket_handler(
-    const SocketHandler& client) {
+
+std::expected<void, std::string> Server::write_to_child(
+    int64_t child_id, std::string_view command, std::string_view message) {
+  auto it{lobbies.find(child_id)};
+  if (it == lobbies.end()) return std::unexpected("Lobby not found");
+  SocketHandler& handler{it->second};
+  return handler.write_to_user(command)
+      .transform_error([](const auto& error) {
+        log_and_forward(error);
+        return std::format("Cant send message to child process: {}", error);
+      })
+      .and_then([&handler, &message]() -> Ev {
+        return handler.write_to_user(message).transform_error(
+            [](const auto& error) {
+              log_and_forward(error);
+              return std::format("Cant send message to child process: {}",
+                                 error);
+            });
+      });
+}
+
+void Server::erase_socket_handler(SocketHandler& client) {
+  epoll_handler.remove_socket(&client).transform_error(log_and_forward);
   std::erase_if(sockets, [&client](const auto& s) {
     return s.get()->get_socket() == client.get_socket();
   });
-  return {};
 }
 
 void Server::handle_zombie_pocesses() {
-  LOG("Server::handle_zombie_pocesses()");
   int status;
   pid_t pid;
   while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
--- application.h
+++ application.h
@@ -8,6 +8,7 @@
 #include <memory>
 #include <ranges>
 #include <unordered_map>
+
 #include "config.h"
 #include "data_storage.h"
 #include "logger.h"
@@ -21,27 +22,34 @@ class Application {
   virtual void run() = 0;
   virtual ~Application() = default;
 
+ protected:
+  EpollHandler epoll_handler;
+  std::vector<std::unique_ptr<SocketHandler>> sockets;
+
  private:
 };
 class Server : public Application {
  public:
   Server() = default;
-  std::expected<void, std::string> init();
+  std::expected<void, std::string> init();  // init() for Server
+  std::expected<void, std::string> init(
+      SocketHandler& parrent_socket);  // init() for Lobby
   virtual void run();
 
  private:
-  EpollHandler epoll_handler;
-  std::vector<std::unique_ptr<SocketHandler>> sockets;
-  std::unordered_map<int64_t, SocketHandler> lobbies;
-  // std::unique_ptr<std::list<Lobbies>> lobbies;
-
-  // std::map<const int, std::unique_ptr<Observer>> observers;
-  std::expected<void, std::string> handle_client_cmd(
-      const SocketHandler& client, std::string_view command);
-  std::expected<void, std::string> create_lobby(const SocketHandler& client);
-  std::expected<void, std::string> erase_socket_handler(
-      const SocketHandler& client);
+  void process_events(std::vector<SocketHandler*>& events);
+  void process_server_socket(SocketHandler* handler);
+  void process_client_socket(SocketHandler* handler);
+  std::expected<void, std::string> handle_client_cmd(SocketHandler& client,
+                                                     std::string_view command);
+  std::expected<void, std::string> create_lobby(SocketHandler& client);
+  std::expected<void, std::string> write_to_child(int64_t child_id,
+                                                  std::string_view command,
+                                                  std::string_view message);
+  void erase_socket_handler(SocketHandler& client);
   void handle_zombie_pocesses();
+
+  std::unordered_map<int64_t, SocketHandler> lobbies;
 };
 class Client : public Application {
  public:
@@ -53,8 +61,6 @@ class Lobby : public Application {
   virtual void run() override;
 
  private:
-  EpollHandler epoll_handler;
-  std::vector<std::unique_ptr<SocketHandler>> sockets;
   std::expected<void, std::string> handle_client_cmd(
       const SocketHandler& client, std::string_view command);
   std::expected<void, std::string> accept_socket(const SocketHandler&);
