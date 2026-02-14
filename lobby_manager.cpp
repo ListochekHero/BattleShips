32c9876dbd5269939967edc2371155436a2ec1ab
ListochekHero <artoryasabysswalker@gmail.com>
2026-02-14 02:18:17 +0200

    New class LobbyManager - all lobby related logic including child process creating migrated here

---
+++ lobby_manager.cpp
@@ -0,0 +1,45 @@
+#include "lobby_manager.h"
+#include "network_routine.h"
+#include <csignal>
+
+namespace bsm {
+
+std::expected<const LobbyView, Error> LobbyManager::find(int64_t lobby_id) {
+  auto it = lobbies_.find(lobby_id);
+  if (it == lobbies_.end()) {
+    return std::unexpected(Error{"Lobby with given id doesn`t exist"});
+  }
+  return LobbyView{lobby_id, it->second.control_connection};
+}
+
+std::expected<LobbyProcess, Error> LobbyManager::spawn_lobby() {
+  int sv[2];
+  if (socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_NONBLOCK, 0, sv) == -1)
+    return std::unexpected(make_error_c("Failed to create socketpair: {}"));
+  pid_t pid = fork();
+  if (pid == -1)
+    return std::unexpected(make_error_c("Failed to fork: {}"));
+  if (pid == 0) {
+    close(sv[1]);
+    std::string socket_string{std::to_string(sv[0])};
+    execl("/home/listochekhero/projects/battleships/build-debug/server",
+          "./lobby", socket_string.c_str(), NULL);
+    perror("execl failed");
+    _exit(127);
+  }
+  close(sv[0]);
+  return LobbyProcess{pid, sv[1]};
+}
+
+std::expected<const LobbyView, Error>
+LobbyManager::attach(pid_t pid, ConnectionView control_connection) {
+  int64_t lobby_id{generate_conn_code()};
+  auto [it, inserted] =
+      lobbies_.try_emplace(lobby_id, LobbyEntry{pid, control_connection});
+  if (!inserted) {
+    kill(pid, SIGKILL);
+    return std::unexpected(Error{"Unable to emplace lobby into map", us_e::CANT_CREATE_LOBBY
+  });
+}
+}
+}
---
+++ lobby_manager.h
@@ -0,0 +1,41 @@
+#ifndef LOBBY_MANAGER_H
+#define LOBBY_MANAGER_H
+
+#include "data_storage.h"
+#include "network_routine.h"
+#include "utility.h"
+#include <cstdint>
+#include <expected>
+#include <unordered_map>
+
+namespace bsm {
+
+struct LobbyProcess {
+  pid_t pid;
+  int control_socket;
+};
+
+struct LobbyEntry {
+  pid_t pid;
+  ConnectionView control_connection;
+};
+
+struct LobbyView {
+   int64_t lobby_id;
+   ConnectionView control_connection;
+};
+
+class LobbyManager {
+public:
+  std::expected<const LobbyView, Error> find(int64_t lobby_id);
+  std::expected<LobbyProcess, Error> spawn_lobby();
+  std::expected<const LobbyView, Error> attach(pid_t pid,
+                                         ConnectionView control_connection);
+
+private:
+  std::unordered_map<int64_t, LobbyEntry> lobbies_;
+};
+
+} // namespace bsm
+
+#endif
