3fbce841356c4beaf4730e8f60c2a58a20b68c5c
ListochekHero <artoryasabysswalker@gmail.com>
2026-02-19 05:28:40 +0200

    Cosmetic changes to fields names, proper attach() method, register_client() method now uses two separated methods() to properly register clients.

--- network_routine.cpp
+++ network_routine.cpp
@@ -14,7 +14,7 @@ Ev NetworkEngine::init() {
     LOG(result.error().message);
     return std::unexpected(Error{"Unable to add init socket to pool"});
   };
-  return this->socket_pool[0]
+  return this->socket_pool_[0]
       .handler->setup_listener(Config::instance().get_line("port"))
       .or_else([](auto&& error) -> Ev {
         LOG(error.message);
@@ -28,18 +28,19 @@ Ev NetworkEngine::init(int parrent_socket, end_point_e socket_type) {
     LOG(result.error().message);
     return std::unexpected(Error{"Unable to add parent socket to pool"});
   }
-  this->socket_pool[0].handler->set_socket_type(socket_type_e::IPC);
+
+  socket_pool_[0].type = end_point_e::PARENT;
   return init_epoll_wrapper();
 }
 
 void NetworkEngine::set_message_handler(MessageHandler h) {
-  on_message_callback = h;
+  on_message_callback_ = h;
 }
 
 void NetworkEngine::run() {
   while (true) {
     [[maybe_unused]]
-    auto __ = epoll_handler.wait_for_events(MAX_EVENTS)
+    auto __ = epoll_handler_.wait_for_events(MAX_EVENTS)
                   .and_then([this](auto&& event_slots) -> Ev {
                     process_events(event_slots);
                     return {};
@@ -55,31 +56,38 @@ void NetworkEngine::run() {
 
 void NetworkEngine::send_message_to(const ConnectionView& conn_view,
                                     const OutgoingMessage& message) {
-  socket_pool[conn_view.get_slot()].handler->write_to_user(message);
+  socket_pool_[conn_view.get_slot()].handler->write_to_user(message);
 }
 
 void NetworkEngine::set_status_to(ConnectionView& conn_view,
                                   socket_status_e status) {
-  socket_pool[conn_view.get_slot()].handler->set_socket_status(status);
+  socket_pool_[conn_view.get_slot()].handler->set_socket_status(status);
 }
 
 // ConnectionView NetworkEngine::attach(int socket) { register_client(socket); }
 
-ConnectionView NetworkEngine::attach(int socket, end_point_e socket_type) {
-  register_client(socket, socket_type);
+std::expected<ConnectionView, Error>
+NetworkEngine::attach(int socket, end_point_e socket_type) {
+  auto slot_index = register_client(socket, socket_type);
+  if (slot_index) {
+    return ConnectionView{*slot_index};
+  } else {
+    LOG(slot_index.error().message);
+    return std::unexpected(Error{"Unable to attach new client to engine"});
+  }
 }
 
 Ev NetworkEngine::init_epoll() {
-  return this->epoll_handler.init()
+  return this->epoll_handler_.init()
       .or_else([](auto&& error) -> Ev {
         LOG(error.message);
         return std::unexpected(Error{"Cant init epoll_handler"});
       })
       .and_then([this]() -> Ev {
-        return this->epoll_handler.add_socket(*socket_pool[0].handler)
+        return this->epoll_handler_.add_socket(*socket_pool_[0].handler, 0)
             .or_else([](auto&& error) -> Ev {
               LOG(error.message);
-              return std::unexpected(Error{"Cant add socket to epoll"});
+              return std::unexpected(Error{"Cant add server socket to epoll"});
             });
       });
 }
@@ -95,10 +103,10 @@ std::expected<size_t, Error>
 NetworkEngine::emplace_new_entry_to_pool(int socket_fd,
                                          end_point_e socket_type) {
   try {
-    socket_pool.emplace_back(
-        SlotEntry{std::make_unique<SocketHandler>(socket_fd), socket_type});
-    size_t occupied_slot{socket_pool.size() - 1};
-    socket_pool.back().handler->set_occupied_slot(occupied_slot);
+    size_t occupied_slot{socket_pool_.size()};
+    socket_pool_.emplace_back(
+        SlotEntry{std::make_unique<SocketHandler>(socket_fd), socket_type,
+                  occupied_slot});
     return occupied_slot;
   } catch (const std::exception& e) {
     LOG(e.what());
@@ -108,10 +116,9 @@ NetworkEngine::emplace_new_entry_to_pool(int socket_fd,
 std::expected<size_t, Error>
 NetworkEngine::emplace_new_entry_to_pool(end_point_e socket_type) {
   try {
-    socket_pool.emplace_back(
-        SlotEntry{std::make_unique<SocketHandler>(), socket_type});
-    size_t occupied_slot{socket_pool.size() - 1};
-    socket_pool.back().handler->set_occupied_slot(occupied_slot);
+    size_t occupied_slot{socket_pool_.size()};
+    socket_pool_.emplace_back(SlotEntry{std::make_unique<SocketHandler>(),
+                                        socket_type, occupied_slot});
     return occupied_slot;
   } catch (const std::exception& e) {
     LOG(e.what());
@@ -120,7 +127,7 @@ NetworkEngine::emplace_new_entry_to_pool(end_point_e socket_type) {
 }
 std::expected<size_t, Error> NetworkEngine::add_to_socket_pool() {
   return emplace_new_entry_to_pool(end_point_e::SERVER)
-      .transform_error([](auto&& error) {
+      .transform_error([](auto error) {
         LOG(error.message);
         error.message = "Unable to increase socket pool size";
         return error;
@@ -129,7 +136,7 @@ std::expected<size_t, Error> NetworkEngine::add_to_socket_pool() {
 std::expected<size_t, Error>
 NetworkEngine::add_to_socket_pool(int socket_fd, end_point_e socket_type) {
   return emplace_new_entry_to_pool(socket_fd, socket_type)
-      .transform_error([](auto&& error) {
+      .transform_error([](auto error) {
         LOG(error.message);
         error.message = "Unable to add given socket to socket pool";
         return error;
@@ -138,17 +145,17 @@ NetworkEngine::add_to_socket_pool(int socket_fd, end_point_e socket_type) {
 
 CommandStatus NetworkEngine::free_slot_entry(SlotEntry& slot_entry) {
   slot_entry.handler->reset_to_empty();
-  avaiable_slots.emplace_back(slot_entry.slot);
+  avaiable_slots_.emplace_back(slot_entry.slot);
   return {};
 }
 
-std::optional<size_t>
+std::expected<size_t, Error>
 NetworkEngine::find_spot_for_new_client(int client_socket,
                                         end_point_e socket_type) {
-  if (!avaiable_slots.empty()) {
-    size_t slot = avaiable_slots.back();
-    avaiable_slots.pop_back();
-    SlotEntry* entry{&socket_pool[slot]};
+  if (!avaiable_slots_.empty()) {
+    size_t slot = avaiable_slots_.back();
+    avaiable_slots_.pop_back();
+    SlotEntry* entry{&socket_pool_[slot]};
     entry->handler->reset_with_new(client_socket);
     entry->type = socket_type;
     return slot;
@@ -157,7 +164,7 @@ NetworkEngine::find_spot_for_new_client(int client_socket,
     if (!new_entry_slot) {
       if (!new_entry_slot) {
         LOG(new_entry_slot.error().message);
-        return std::nullopt;
+        return std::unexpected(Error{"Unable to find spot for new client"});
       }
     }
     return new_entry_slot.value();
@@ -165,7 +172,7 @@ NetworkEngine::find_spot_for_new_client(int client_socket,
 }
 
 Ev NetworkEngine::subscribe_to_events(SlotEntry& slot_entry) {
-  return epoll_handler.add_socket(*slot_entry.handler)
+  return epoll_handler_.add_socket(*slot_entry.handler, slot_entry.slot)
       .or_else([&](auto&& error) -> Ev {
         LOG(error.message);
         return std::unexpected(
@@ -174,7 +181,7 @@ Ev NetworkEngine::subscribe_to_events(SlotEntry& slot_entry) {
 }
 
 void NetworkEngine::unsubscribe_from_events(SlotEntry& slot_entry) {
-  drop_result(epoll_handler.remove_socket(*slot_entry.handler)
+  drop_result(epoll_handler_.remove_socket(*slot_entry.handler)
                   .or_else([](auto&& error) -> Ev {
                     LOG(error.message);
                     return {};
@@ -186,41 +193,45 @@ void NetworkEngine::release_client(SlotEntry& slot_entry) {
   return;
 }
 
-void NetworkEngine::register_client(int client_socket,
-                                    end_point_e socket_type) {
+std::expected<size_t, Error>
+NetworkEngine::register_client(int client_socket, end_point_e socket_type) {
   auto new_client_slot = find_spot_for_new_client(client_socket, socket_type);
   if (!new_client_slot) {
-    LOG("Unable to register new client");
-    return;
+    LOG(new_client_slot.error().message);
+    return std::unexpected(Error{"Unable to register new client"});
   }
-  SlotEntry& new_client_entry{socket_pool[new_client_slot.value()]};
-  if (auto result = subscribe_to_events(new_client_entry); !result) {
-    LOG(result.error().message);
-    if (auto r = send_error_reply(new_client_entry, result.error()); !r) {
-      LOG(r.error().message);
-    }
-    free_slot_entry(new_client_entry);
-    LOG("Unable to add new client to epoll");
-  }
+  SlotEntry& new_client_entry{socket_pool_[new_client_slot.value()]};
+  return subscribe_to_events(new_client_entry)
+      .transform([&]() { return new_client_entry.slot; })
+      .or_else([&](auto&& error) -> std::expected<size_t, Error> {
+        LOG(error.message);
+        if (auto r = send_error_reply(new_client_entry, error); !r) {
+          LOG(r.error().message);
+        }
+        free_slot_entry(new_client_entry);
+        return std::unexpected(Error{"Unable to add new client to epoll"});
+      });
 }
 
 void NetworkEngine::register_clients(std::vector<int>& new_clients) {
   for (int client : new_clients) {
-    register_client(client, end_point_e::CLIENT);
+    if (auto result = register_client(client, end_point_e::CLIENT); !result) {
+      LOG(result.error().message);
+    }
   }
 }
 
 void NetworkEngine::run_deferred_actions() {
-  for (auto& action : deferred_actions) {
+  for (auto& action : deferred_actions_) {
     action->run(*this);
   }
-  deferred_actions.clear();
+  deferred_actions_.clear();
 }
 
 void NetworkEngine::process_events(std::vector<size_t>& event_slots) {
   for (size_t slot : event_slots) {
-    SlotEntry& slot_entry{socket_pool[slot]};
-    if (slot_entry.handler->get_socket_type() == socket_type_e::SERVER) {
+    SlotEntry& slot_entry{socket_pool_[slot]};
+    if (slot_entry.type == end_point_e::SERVER) {
       process_server_socket(slot_entry);
     } else {
       process_client_socket(slot_entry);
@@ -269,8 +280,8 @@ CommandStatus NetworkEngine::process_message(SlotEntry& slot_entry,
   }
   ConnectionView connection{slot_entry.slot};
   CommandContext context{connection, message, slot_entry.type};
-  CommandStatus cmd_status =
-      on_message_callback(context); //<-CallBack to Server, process user message
+  CommandStatus cmd_status = on_message_callback_(
+      context); //<-CallBack to Server, process user message
   if (cmd_status.error) {
     LOG(cmd_status.error->message);
     LOG("Unable to handle client command: " + context.message.payload);
@@ -280,7 +291,8 @@ CommandStatus NetworkEngine::process_message(SlotEntry& slot_entry,
   return cmd_status;
 }
 
-Ev NetworkEngine::send_error_reply(const SlotEntry& slot_entry, Error& error) {
+Ev NetworkEngine::send_error_reply(const SlotEntry& slot_entry,
+                                   const Error& error) {
   return slot_entry.handler->write_to_user({{user_message(error.user_code)}})
       .or_else([](auto&& error) -> Ev {
         LOG(error.message);
--- network_routine.h
+++ network_routine.h
@@ -14,7 +14,8 @@ enum class end_point_e : uint8_t {
   NONE = 0,
   CLIENT = 1 << 0,
   LOBBY = 1 << 1,
-  SERVER = 1 << 2,
+  PARENT = 1 << 2,
+  SERVER = 1 << 3,
 };
 
 struct SlotEntry {
@@ -36,7 +37,8 @@ public:
                        const OutgoingMessage& message);
   void set_status_to(ConnectionView& conn_view, socket_status_e status);
   // ConnectionView attach(int socket);
-  ConnectionView attach(int socket, end_point_e socket_type);
+  std::expected<ConnectionView, Error> attach(int socket,
+                                              end_point_e socket_type);
 
 private:
   Ev init_epoll();
@@ -49,25 +51,26 @@ private:
   std::expected<size_t, Error> add_to_socket_pool(int socket_fd,
                                                   end_point_e socket_type);
   CommandStatus free_slot_entry(SlotEntry& slot_entry);
-  std::optional<size_t> find_spot_for_new_client(int client_socket,
-                                                 end_point_e socket_type);
+  std::expected<size_t, Error>
+  find_spot_for_new_client(int client_socket, end_point_e socket_type);
   Ev subscribe_to_events(SlotEntry& slot_entry);
   void unsubscribe_from_events(SlotEntry& slot_entry);
   void release_client(SlotEntry& slot_entry);
-  void register_client(int client_socket, end_point_e socket_type);
+  std::expected<size_t, Error> register_client(int client_socket,
+                                               end_point_e socket_type);
   void register_clients(std::vector<int>& new_clients);
   void run_deferred_actions();
   void process_events(std::vector<size_t>& event_slots);
   void process_server_socket(SlotEntry& slot_entry);
   void process_client_socket(SlotEntry& slot_entry);
   CommandStatus process_message(SlotEntry& slot_entry, ReadResult& message);
-  Ev send_error_reply(const SlotEntry& slot_entry, Error& error);
+  Ev send_error_reply(const SlotEntry& slot_entry, const Error& error);
 
-  std::vector<size_t> avaiable_slots;
-  MessageHandler on_message_callback;
-  EpollHandler epoll_handler;
-  std::vector<SlotEntry> socket_pool;
-  std::vector<std::unique_ptr<DeferredAction>> deferred_actions;
+  std::vector<size_t> avaiable_slots_;
+  MessageHandler on_message_callback_;
+  EpollHandler epoll_handler_;
+  std::vector<SlotEntry> socket_pool_;
+  std::vector<std::unique_ptr<DeferredAction>> deferred_actions_;
 };
 
 class ConnectionView {
