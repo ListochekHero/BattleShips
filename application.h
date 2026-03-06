2e5e660504d0f65374d8c8a8ee3bf1e44dc99092
ListochekHero <artoryasabysswalker@gmail.com>
2026-03-06 03:52:07 +0200

    ...

--- application.h
+++ application.h
@@ -28,41 +28,6 @@ public:
 protected:
   Dispatcher& dispatcher();
   NetworkEngine& net_engine();
-
-  template < typename Variant, typename Fn>
-  CommandStatus handle_cmd_impl(Fn&& fn, CommandContext& context) {
-    LOG(std::format("Command to handle: {}", context.message.payload));
-    auto action_to_execute = dispatcher().dispatch(context);
-    auto lobby_action = filter_variant<Variant>(action_to_execute);
-    if (!lobby_action) {
-      lobby_action.error().add_context("error in handle_client_cmd() method");
-    }
-    return std::visit(
-        [&](auto&& lobby_action) -> CommandStatus {
-          return std::invoke(fn, lobby_action, context);
-        },
-        *lobby_action);
-  }
-  template <typename Variant, typename InitFn, typename OnReadyFn,
-            typename LambdFn>
-    requires requires(InitFn init) {
-      { init() } -> std::convertible_to<std::expected<ConnectionView, Error>>;
-    } && std::invocable<InitFn> && std::invocable<OnReadyFn, ConnectionView&>
-  Ev init_common2(InitFn&& init_net_engine, OnReadyFn&& post_engine_init,
-                  LambdFn&& lambd_fn, std::string_view error_context) {
-    net_engine_.set_message_handler([&](CommandContext context) {
-      return handle_cmd_impl<Variant>(lambd_fn, context);
-    });
-    auto r = std::invoke(init_net_engine);
-    if (!r) {
-      r.error().add_context(error_context.data());
-      r.error().add_context("Unable to execute init_common()");
-      return std::unexpected(std::move(r).error());
-    }
-    std::invoke(post_engine_init, *r);
-    return {};
-  }
-
   virtual CommandStatus handle_client_cmd(CommandContext& context) = 0;
   template <typename InitFn, typename OnReadyFn>
     requires requires(InitFn init) {
@@ -95,8 +60,6 @@ public:
   void cleanup_slot(size_t slot);
 
 private:
-  template <typename T>
-  CommandStatus handle_client_cmd2(T t, CommandContext& context);
   CommandStatus accept_socket_from_parent(const CommandContext& context);
   CommandStatus send_connection_code(const CommandContext& context);
   CommandStatus join_lobby(const CommandContext& context);
