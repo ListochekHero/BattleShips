290f3bad2bb32faff1d0bdd82af1fad2c06d726b
ListochekHero <artoryasabysswalker@gmail.com>
2026-04-04 22:33:08 +0300

    Rework of Module system, now it adds interface for attaching module to Shceduler

--- modules.cpp
+++ modules.cpp
@@ -2,24 +2,4 @@
 
 namespace bsm {
 
-void NetworkModule::setup(Scheduler& scheduler) {
-  scheduler.add_executor("network_task",
-                         [this](std::unique_ptr<TaskContext> context) {
-                           NetworkTaskContext* network_context =
-                               static_cast<NetworkTaskContext*>(context.get());
-                           net_engine_.process_client(network_context->slot);
-                         });
-  size_t slot =
-      scheduler.add_co_task([this]() { return net_engine_.run_co(); });
-  scheduler.schedule_co_task(slot, [&scheduler](
-                                       std::coroutine_handle<> te_handle) {
-    co_handle_type co_handle =
-        co_handle_type::from_address(te_handle.address());
-    while (true) {
-      co_handle.resume();
-      scheduler.push_task("network_task", std::make_unique<NetworkTaskContext>(
-                                              co_handle.promise().latest_slot));
-    }
-  });
-}
 } // namespace bsm
--- modules.h
+++ modules.h
@@ -1,26 +1,14 @@
 #ifndef MODULES_H
 #define MODULES_H
 
-#include "dispatcher.h"
-#include "network_routine.h"
 #include "scheduler.h"
-#include <array>
-#include <atomic>
-#include <cstdint>
-#include <limits>
 
 namespace bsm {
 
-struct Module {
+class Module {
 public:
-  virtual void setup(Scheduler& scheduler) = 0;
+  virtual void attach_to_scheduler(Scheduler& scheduler) = 0;
   virtual ~Module() = default;
-};
-
-struct NetworkModule : Module {
-  void setup(Scheduler& scheduler) override;
-
-  NetworkEngine net_engine_;
 };
 
 } // namespace bsm
