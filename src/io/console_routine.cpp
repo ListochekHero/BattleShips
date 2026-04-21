7f1d00c54c2557cda03aa53ae7e3aab1eac1bd65
ListochekHero <artoryasabysswalker@gmail.com>
2026-04-22 01:17:21 +0300

    Removed just another unnecessary code

--- src/io/console_routine.cpp
+++ src/io/console_routine.cpp
@@ -6,44 +6,15 @@
 
 namespace bsm {
 
-void ConsoleHandler::attach_to_scheduler(Scheduler& scheduler) {
-  size_t slot = scheduler.add_co_task([this]() { return run_co(); });
-  scheduler.schedule_co_task(
-      slot, [&scheduler](std::coroutine_handle<> te_handle) {
-        console_co_handle co_handle =
-            console_co_handle::from_address(te_handle.address());
-        while (true) {
-          co_handle.resume();
-          scheduler.push_task("console_task",
-                              std::make_unique<ConsoleTaskContext>(
-                                  std::move(co_handle.promise().latest_input)));
-        }
-      });
-}
-
 void ConsoleHandler::run() {
   std::string user_cmd;
   while (std::getline(std::cin, user_cmd)) {
-    // on_input_callback(std::move(user_cmd));
     auto ptr{new std::string(std::move(user_cmd))};
     console_raw_tasks_.push(ptr);
     available_task_tags_.push(new task_tag_e(task_tag_e::CONSOLE));
     if (*ptr == "\\quit")
       break;
   }
-}
-
-console_co_handle ConsoleHandler::run_co() {
-  std::string user_input;
-  while (std::getline(std::cin, user_input)) {
-    co_yield (std::move(user_input));
-    if (user_input == "\\quit")
-      break;
-  }
-}
-
-void ConsoleHandler::set_input_handler(InputHandler h) {
-  on_input_callback = h;
 }
 
 } // namespace bsm
--- src/io/console_routine.h
+++ src/io/console_routine.h
@@ -4,51 +4,22 @@
 #include "core/scheduler.h"
 #include "interfaces/modules.h"
 
-#include <coroutine>
-#include <functional>
 #include <string>
 
 namespace bsm {
-
-struct console_promise;
-using console_co_handle = std::coroutine_handle<console_promise>;
-
-struct console_promise {
-  std::string latest_input{};
-  console_co_handle get_return_object() {
-    return console_co_handle::from_promise(*this);
-  }
-  std::suspend_always initial_suspend() noexcept { return {}; }
-  std::suspend_always final_suspend() noexcept { return {}; }
-  std::suspend_always yield_value(std::string user_input) {
-    latest_input = std::move(user_input);
-    return {};
-  }
-  void unhandled_exception() {}
-};
 
 class ConsoleHandler : public Module {
 public:
   ConsoleHandler(AtomicQueue<std::string>& console_q_,
                  AtomicQueue<task_tag_e>& available_q_)
       : console_raw_tasks_(console_q_), available_task_tags_(available_q_) {};
-
-  // void attach_to_scheduler(Scheduler& scheduler) override;
   void run();
-  console_co_handle run_co();
-  using InputHandler = std::function<void(std::string)>;
-  void set_input_handler(InputHandler h);
 
 private:
   AtomicQueue<std::string>& console_raw_tasks_;
   AtomicQueue<task_tag_e>& available_task_tags_;
-  InputHandler on_input_callback;
 };
 
 } // namespace bsm
 
-template <>
-struct std::coroutine_traits<bsm::console_co_handle, bsm::ConsoleHandler&> {
-  using promise_type = bsm::console_promise;
-};
 #endif
