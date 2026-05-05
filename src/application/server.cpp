08afd1edd8a12a5aa9a79984ebd487b72216f1a2
ListochekHero <artoryasabysswalker@gmail.com>
2026-05-05 19:25:58 +0300

    Added method init_lobby(), more error handling, better logs, naming changes plus minor fixes

--- src/application/server.cpp
+++ src/application/server.cpp
@@ -1,23 +1,31 @@
 #include "server.h"
 
+#include "core/dispatcher.h"
+#include "protocol/lobby_types.h"
 #include "utility/data_storage.h"
 #include "utility/error.h"
 #include "utility/utility.h"
 #include <csignal>
+#include <expected>
 #include <sys/wait.h>
 
 namespace bsm {
 
-auto Server::v_init(end_point_e socket_type, int parrent_socket) -> Ev {
-  return Application::init(socket_type, parrent_socket)
-      .transform([](auto) -> void {});
+auto Server::init(end_point_e socket_type, int socket) -> std::optional<Error> {
+  auto init_result = Application::init_app(socket_type, socket);
+  if (!init_result) {
+    return std::move(init_result)
+        .error()
+        .add_context("Unable to init Server:: failed to init Application");
+  }
+  return std::nullopt;
 }
 
 void Server::run() {
   scheduler().add_and_run_producer(
       [this]() -> void { network_engine().run_event_loop(); });
   scheduler().run_workers();
-  scheduler().get_co_by_tag(task_tag_e::SCHEDULER).resume();
+  scheduler().get_co_handle_by_tag(task_tag_e::SCHEDULER).resume();
 }
 
 void Server::handle_zombie_pocesses() {
@@ -31,17 +39,15 @@ void Server::handle_zombie_pocesses() {
   }
 }
 
-auto Server::handle_client_cmd(const ActionContext& context) -> ActionResult {
-  LOG(std::format("Command to handle: {}", context.received_message.payload));
-  CommandInfo command_info =
-      bsm::Dispatcher::dispatch(context.received_message);
-  auto server_action = filter_variant<ServerAction>(command_info.parsed_cmd);
+auto Server::handle_action(const ActionContext& context) -> ActionResult {
+  ActionInfo action_info = bsm::Dispatcher::dispatch(context.received_message);
+  auto server_action = filter_variant<ServerAction>(action_info.variant);
   if (!server_action) {
     return {
         .conn_status = ConnectionStatus::KEEP,
         .error = std::move(server_action)
                      .error()
-                     .add_context("Command not allowed in this context"),
+                     .add_context("Action in not allowed in this context"),
     };
   }
   return std::visit(
@@ -53,44 +59,67 @@ auto Server::handle_client_cmd(const ActionContext& context) -> ActionResult {
 
 auto Server::execute_action(const CreateLobby& /*unused*/,
                             const ActionContext& context) -> ActionResult {
-  auto lobby_view{request_lobby()};
+  auto lobby_view{create_lobby()};
   if (!lobby_view) {
+    lobby_view.error().add_context(
+        "Unable to create lobby: failed to request lobby");
     return {
         .conn_status = ConnectionStatus::KEEP,
         .error = std::move(lobby_view.error()),
         .user_code = user_error_e::CANT_CREATE_LOBBY,
     };
   }
-  network_engine().send_message_to(
-      lobby_view->control_connection,
-      {
-          .payloads = {std::to_string(lobby_view->lobby_id)},
-          .msg_type = message_type_e::LOBBY_ID,
-      });
+  if (auto send_error = init_lobby(*lobby_view)) {
+    send_error->add_context("Unable to create lobby:L failed to init lobby");
+    return {
+        .conn_status = ConnectionStatus::KEEP,
+        .error = std::move(send_error),
+        .user_code = user_error_e::CANT_CREATE_LOBBY,
+    };
+  }
   return perform_transfer(*lobby_view, context);
 }
 
-auto Server::request_lobby() -> std::expected<LobbyView, Error> {
-  std::optional<Error> error;
-  auto proc = bsm::LobbyManager::spawn_lobby();
-  if (!proc) {
-    error = proc.error();
-  }
-  auto lobby_ipc = network_engine().attach_socket(proc->control_socket,
-                                                  end_point_e::TO_LOBBY);
-  if (!lobby_ipc) {
-    error = lobby_ipc.error();
-  }
-  auto lobby_handler = lobby_manager_.attach(proc->pid, *lobby_ipc);
-  if (!lobby_handler) {
-    error = lobby_handler.error();
-  }
-  if (error) {
+auto Server::create_lobby() -> std::expected<LobbyView, Error> {
+  auto spawn_result = bsm::LobbyManager::spawn_lobby();
+  if (!spawn_result) {
     return std::unexpected(
-        error->add_context("Unable to setup new lobby for request"));
+        std::move(spawn_result)
+            .error()
+            .add_context("Unable to create lobby: failed to spawn lobby"));
   }
-  return lobby_handler;
+  auto attach_result = network_engine().attach_socket(
+      spawn_result->control_socket, end_point_e::TO_LOBBY);
+  if (!attach_result) {
+    return std::unexpected(std::move(attach_result)
+                               .error()
+                               .add_context("Unable to create lobby: failed to "
+                                            "attach lobby to NetworkEngine"));
+  }
+  auto lobby_view = lobby_manager_.attach(spawn_result->pid, *attach_result);
+  if (!lobby_view) {
+    return std::unexpected(std::move(lobby_view)
+                               .error()
+                               .add_context("Unable to create lobby: failed to "
+                                            "attach lobby to LobbyManager"));
+  }
+  return lobby_view;
 }
+
+auto Server::init_lobby(const LobbyView& lobby_view) -> std::optional<Error> {
+  auto send_error = network_engine().send_message_to(
+      lobby_view.control_connection,
+      {
+          .payloads = {std::to_string(lobby_view.lobby_id)},
+          .type = message_type_e::LOBBY_ID,
+      });
+  if (send_error) {
+    return send_error->add_context(
+        "Unable to init lobby: failed to send lobby id");
+  }
+  return std::nullopt;
+}
+
 auto Server::perform_transfer(const LobbyView& recipient, ActionContext context)
     -> ActionResult {
   auto transfer_result{
@@ -110,7 +139,8 @@ auto Server::execute_action(const JoinLobby& /*unused*/,
                             const ActionContext& context) -> ActionResult {
   auto parse_result = JoinLobbyCode::parse(context.received_message.payload);
   if (!parse_result) {
-    parse_result.error().add_context("Unable to parse connection code");
+    parse_result.error().add_context(
+        "Unable to join lobby: failed to parse connection code");
     return {
         .conn_status = ConnectionStatus::KEEP,
         .error = std::move(parse_result).error(),
@@ -119,7 +149,8 @@ auto Server::execute_action(const JoinLobby& /*unused*/,
   }
   auto lobby_view = lobby_manager_.find(parse_result->int_code);
   if (!lobby_view) {
-    lobby_view.error().add_context("Unable to find lobby with given id");
+    lobby_view.error().add_context(
+        "Unable to join lobby: failed to find lobby");
     return {
         .conn_status = ConnectionStatus::KEEP,
         .error = std::move(lobby_view).error(),
@@ -133,14 +164,15 @@ auto Server::execute_action(const ChatMessage& /*unused*/,
                             const ActionContext& context) -> ActionResult {
   auto parse_result = parse(context.received_message.payload);
   if (!parse_result) {
-    parse_result.error().add_context("Unable to parse client chat message");
+    parse_result.error().add_context(
+        "Unable to process chat message: failed to parse incoming message");
     return {
         .conn_status = ConnectionStatus::KEEP,
         .error = std::move(parse_result).error(),
     };
   }
   network_engine().send_message(
-      {.payloads = {*parse_result}, .msg_type = message_type_e::PRINTABLE},
+      {.payloads = {*parse_result}, .type = message_type_e::PRINTABLE},
       [](const auto& meta) -> auto {
         return meta.type == end_point_e::TO_CLIENT;
       });
@@ -149,12 +181,20 @@ auto Server::execute_action(const ChatMessage& /*unused*/,
 
 auto Server::execute_action(const GeneralAction& /*unused*/,
                             const ActionContext& context) -> ActionResult {
-  network_engine().send_message_to(
+  auto send_error = network_engine().send_message_to(
       context.pending_view,
       {
           .payloads = {user_message(user_error_e::UNKNOWN_COMMAND)},
-          .msg_type = message_type_e::PRINTABLE,
+          .type = message_type_e::PRINTABLE,
       });
+  if (send_error) {
+    send_error->add_context(
+        "Unable to process general action: failed to send answer");
+    return {
+        .conn_status = ConnectionStatus::RELEASE,
+        .error = std::move(send_error),
+    };
+  }
   return {.conn_status = ConnectionStatus::KEEP};
 }
 
--- src/application/server.h
+++ src/application/server.h
@@ -14,17 +14,19 @@ class Server : public Application {
 public:
   Server() = default;
   void run() override;
-  auto v_init(end_point_e socket_type, int parrent_socket) -> Ev;
+  auto init(end_point_e socket_type, int parrent_socket)
+      -> std::optional<Error>;
 
 private:
   static void handle_zombie_pocesses();
 
   using ServerAction =
       std::variant<CreateLobby, JoinLobby, ChatMessage, GeneralAction>;
-  auto handle_client_cmd(const ActionContext& context) -> ActionResult override;
+  auto handle_action(const ActionContext& context) -> ActionResult override;
   auto execute_action(const CreateLobby& action_type,
                       const ActionContext& context) -> ActionResult;
-  auto request_lobby() -> std::expected<LobbyView, Error>;
+  auto create_lobby() -> std::expected<LobbyView, Error>;
+  auto init_lobby(const LobbyView& lobby_view) -> std::optional<Error>;
   auto perform_transfer(const LobbyView& recipient, ActionContext context)
       -> ActionResult;
   auto execute_action(const JoinLobby& action_type,
