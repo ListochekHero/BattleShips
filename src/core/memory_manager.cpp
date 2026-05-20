cbbe30e03e6a526afd4f1804915bd3904208e274
ListochekHero <artoryasabysswalker@gmail.com>
2026-05-20 04:56:49 +0300

    Revamp for init method to also allocate storage for meta data, adde method to populate meta storage plus minor fixes

--- src/core/memory_manager.cpp
+++ src/core/memory_manager.cpp
@@ -2,20 +2,26 @@
 #include "utility/error.h"
 #include "utility/utility.h"
 
+#include <atomic>
 #include <cstddef>
+#include <cstdint>
+#include <fcntl.h>
 #include <iostream>
+#include <new>
 #include <sys/mman.h>
 #include <unistd.h>
 
 namespace bsm {
 
-void MemoryManager::init(std::uint64_t memory_amount) {
-  virtual_pool_ = mmap(nullptr, memory_amount, PROT_NONE,
-                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  free_beggins_ = static_cast<char*>(virtual_pool_);
-}
-  void MemoryManager::set_object_size(size_t size){
-
+void MemoryManager::init(std::uint64_t memory_amount,
+                         std::uint64_t object_size) {
+  meta_data_ = static_cast<char*>(mmap(nullptr, sysconf(_SC_PAGE_SIZE),
+                                       PROT_READ | PROT_WRITE,
+                                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+  populate_meta_storage(object_size);
+  virtual_pool_ = static_cast<char*>(mmap(nullptr, memory_amount, PROT_NONE,
+                                          MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+  free_beggins_ = virtual_pool_;
   }
 
 auto MemoryManager::allocate_raw() -> void* {
@@ -29,7 +35,7 @@ auto MemoryManager::allocate_raw() -> void* {
 }
 
 auto MemoryManager::operator[](size_t index) -> void* {
-  void* object_ptr = static_cast<char*>(virtual_pool_) + (index * object_size_);
+  void* object_ptr = virtual_pool_ + (index * object_size_);
   return object_ptr;
 }
 
@@ -38,8 +44,8 @@ auto MemoryManager::allocate_new_node() -> std::optional<Error> {
             << pages_allocated_ << '\n'
             << std::flush;
   if (auto protect_result = mprotect(
-          static_cast<char*>(virtual_pool_) + (pages_allocated_ * page_size_),
-          page_size_ * NODE_SIZE, PROT_READ | PROT_WRITE);
+          virtual_pool_ + (pages_allocated_ * page_size_),
+          page_size_ * meta_storage_layout::NODE_SIZE, PROT_READ | PROT_WRITE);
       protect_result == -1) {
     return make_error_c(
         "Unable to allocate new memory page: failed to allocate "
@@ -48,6 +54,19 @@ auto MemoryManager::allocate_new_node() -> std::optional<Error> {
   pages_allocated_ += NODE_SIZE;
   space_left_ += page_size_ * NODE_SIZE;
   return std::nullopt;
+}
+
+void MemoryManager::populate_meta_storage(std::uint64_t object_size) {
+  new (meta_data_ + meta_storage_layout::FREE_BEGGINS)
+      std::atomic<char*>(virtual_pool_);
+  new (meta_data_ + meta_storage_layout::ACTUAL_MEMORY_END)
+      std::atomic<char*>(virtual_pool_);
+  new (meta_data_ + meta_storage_layout::OBJECT_SIZE)
+      const std::atomic_uint64_t(object_size);
+  new (meta_data_ + meta_storage_layout::PAGE_SIZE)
+      const std::atomic_int64_t(sysconf(_SC_PAGE_SIZE));
+  new (meta_data_ + meta_storage_layout::ACTUAL_PAGES_ALLOCATED)
+      std::atomic_uint64_t(0);
 }
 
 } // namespace bsm
