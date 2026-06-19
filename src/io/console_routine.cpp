f7f2714905b3ce6836e697de9ec1c6408a91a779
ListochekHero <artoryasabysswalker@gmail.com>
2026-06-19 06:26:27 +0300

    Moved to using MManager in queue for ConsoleHandler class

--- src/io/console_routine.cpp
+++ src/io/console_routine.cpp
@@ -13,9 +13,8 @@ namespace bsm {
 void ConsoleHandler::run() {
   std::string user_cmd;
   while (std::getline(std::cin, user_cmd)) {
-    auto* ptr{new std::string(std::move(user_cmd))};
-    console_raw_tasks_.push(ptr);
-    available_task_tags_.push(new task_tag_e(task_tag_e::CONSOLE));
+    console_raw_tasks_.mmanager_push(std::move(user_cmd));
+    available_task_tags_.mmanager_push(task_tag_e::CONSOLE);
     // if (*ptr == "\\quit") {
     //   break;
     // }
